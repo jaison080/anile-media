9ff88e3b413b71c833ddd64cc0dcd45a3d892f2f
jaison080 <jaisondennis080@gmail.com>
2022-05-19 20:22:23 +0530

    Made UI responsive for Mobile view

--- anile-media-website/pages/form.js
+++ anile-media-website/pages/form.js
@@ -1,6 +1,6 @@
-import { TextField,InputLabel,Select,MenuItem, FormLabel, RadioGroup, FormControlLabel, Radio, Modal, FormControl, OutlinedInput } from "@mui/material";
-import { Box } from "@mui/system";
-
+import { TextField,InputLabel,Select,MenuItem, FormLabel, RadioGroup, FormControlLabel, Radio, Modal, FormControl, OutlinedInput, Grid, Typography } from "@mui/material";
+import { Box, flexbox } from "@mui/system";
+import CssBaseline from '@mui/material/CssBaseline';
 import { useRouter } from "next/router";
 import React from'react'
 const style = {
@@ -9,6 +9,9 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
+  height:400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
   p: 4,
 };
 function form()
@@ -19,10 +22,16 @@ function form()
   const handleClose = () => setOpen(false);
   const handleClick = () => router.push('./form');
     const [age, setAge] = React.useState('');
-
+const [age1, setAge1] = React.useState('');
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+  const handleChange1 = (event) => {
+    setAge1(event.target.value);
+  };
+  function handleSubmit(){
+
+  }
 
     return(
         <div className='container'>
@@ -40,27 +49,74 @@ function form()
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       ><Box sx={style}>
-          <div style={{ height: "400px", width:"400px"}}>
       <iframe
         src="https://calendly.com/jaisondennis090/30min"
         width="100%"
         height="100%"
         frameborder="0"
       ></iframe>
-    </div>
         </Box></Modal>
             </div>
-            <div className="form">
-            <div className="form-title">BASIC INFORMATION</div>
-            <TextField className="outlined-basic" label="Name" variant="outlined" />
-            <div className="form-1">
-                <TextField className="outlined-basic-1" label="Email" variant="outlined" />
-            <TextField className="outlined-basic-2" label="Phone No:" variant="outlined" />
-            </div>
-            <TextField className="outlined-basic-3" label="Company/Organization" variant="outlined" />
-            </div>
-            <FormControl sx={{width:'800px',marginTop:'20px'}}>
-        <InputLabel id="demo-multiple-name-label">How did you hear about us?</InputLabel>
+            <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontFamily:'Raleway',
+          }}
+        >
+          <Typography  className="form-title">
+            BASIC INFORMATION
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={3} sx={{display:'flex',justifyContent:'center'}}>
+              <Grid item xs={10}>
+                <TextField
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  name="name"
+                  autoComplete="name"
+                />
+              </Grid>
+              <Grid item xs={10} sm={5}>
+                <TextField
+                  autoComplete="email"
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  type="email"
+                  label="Email"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={10} sm={5}>
+                <TextField
+                  required
+                  fullWidth
+                  id="phone"
+                  label="Phone No:"
+                  autoComplete="phone"
+                />
+              </Grid>
+              
+              <Grid item xs={10}>
+                <TextField
+                  required
+                  fullWidth
+                  name="company"
+                  label="Company / Organization"
+                  id="company"
+                  autoComplete="company"
+                />
+              </Grid>
+              <Grid item xs={10}>
+                <FormControl fullWidth>
+                <InputLabel id="demo-multiple-name-label" >How did you hear about us?</InputLabel>
         <Select
           labelId="demo-multiple-name-label"
           id="demo-multiple-name"
@@ -96,22 +152,41 @@ function form()
               None
             </MenuItem>
         </Select>
-      </FormControl>
-            <div className="form">
-            <div className="form-title">PROJECT DETAILS</div>
-            <FormControl sx={{width:'800px',marginTop:'20px'}}>
-        <InputLabel id="demo-multiple-name-label">How would you like to work with us?</InputLabel>
-        <Select
+        </FormControl>
+              </Grid>
+            </Grid>
+            </Box>
+            </Box>
+             <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            fontFamily:'Raleway',
+            alignItems: 'center',
+          }}
+        >
+          <Typography  className="form-title">
+            PROJECT DETAILS
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ fontFamily:'Raleway',mt: 3 }}>
+            <Grid container spacing={3} sx={{display:'flex',justifyContent:'center'}}>
+               <Grid item xs={10} >
+                <FormControl fullWidth>
+                <InputLabel sx={{ fontFamily:'Raleway'}} id="demo-multiple-name-label">How would you like to work with us?</InputLabel>
+         <Select
           labelId="demo-multiple-name-label"
           id="demo-multiple-name"
-          value={age}
-          onChange={handleChange}
+          value={age1}
+          sx={{ fontFamily:'Raleway'}}
+          onChange={handleChange1}
           input={<OutlinedInput label="How would you like to work with us?" />}
         >
             
             <MenuItem
               key='1'
               value='1'
+              sx={{ fontFamily:'Raleway'}}
             >
               Web Design & Development
 
@@ -119,6 +194,7 @@ function form()
 
             <MenuItem
               key='2'
+              sx={{ fontFamily:'Raleway'}}
               value='2'
             >
               Branding & Identity
@@ -128,12 +204,14 @@ function form()
             <MenuItem
               key='3'
               value='3'
+              sx={{ fontFamily:'Raleway'}}
             >
               Digital Marketing
 
             </MenuItem>
             <MenuItem
               key='4'
+              sx={{ fontFamily:'Raleway'}}
               value='4'
             >
               Custom Web/Mobile App Development
@@ -142,6 +220,7 @@ function form()
             </MenuItem>
             <MenuItem
               key='5'
+              sx={{ fontFamily:'Raleway'}}
               value='5'
             >
               Strategy & Consulting
@@ -150,6 +229,7 @@ function form()
             <MenuItem
               key='6'
               value='6'
+              sx={{ fontFamily:'Raleway'}}
             >
               Ongoing Support
 
@@ -158,39 +238,48 @@ function form()
 
             <MenuItem
               key='0'
+              sx={{ fontFamily:'Raleway'}}
               value='0'
             >
               None
             </MenuItem>
         </Select>
-      </FormControl>
-             <FormLabel className="radio-label">What's your budget ?</FormLabel>
+        </FormControl>
+              </Grid>
+              <Grid item xs={10}>
+                <FormLabel sx={{display:'flex',fontFamily:'Raleway',justifyContent:'center'}}>What's your budget ?</FormLabel>
       <RadioGroup
+      sx={{display:'flex',justifyContent:'center'}}
         row
         aria-labelledby="demo-row-radio-buttons-group-label"
         name="row-radio-group-label"
       >
-        <FormControlLabel value="female"  control={<Radio />} label="$20k-40k" />
-        <FormControlLabel value="male" control={<Radio />} label="$40k-60k" />
-        <FormControlLabel value="other" control={<Radio />} label="$60k-80k" />
-         <FormControlLabel value="other1" control={<Radio />} label="$100k +" />
+        <FormControlLabel sx={{ fontFamily:'Raleway'}} value="female"  control={<Radio />} label="$20k-40k" />
+        <FormControlLabel sx={{ fontFamily:'Raleway'}} value="male" control={<Radio />} label="$40k-60k" />
+        <FormControlLabel sx={{ fontFamily:'Raleway'}} value="other" control={<Radio />} label="$60k-80k" />
+         <FormControlLabel sx={{ fontFamily:'Raleway'}} value="other1" control={<Radio />} label="$100k +" />
       </RadioGroup>
-      <FormLabel className="radio-label-1">Project Details</FormLabel>
+              </Grid>
+            <Grid item xs={10}>
+              <FormLabel sx={{display:'flex',justifyContent:'center', fontFamily:'Raleway'}}>Project Details</FormLabel>
       <TextField
           id="outlined-multiline-static"
           multiline
-          sx={{width:"800px",marginTop:"20px"}}
+          sx={{ fontFamily:'Raleway'}}
+          fullWidth
           rows={10}
         />
+            </Grid>
+             
+            </Grid>
+            </Box>
+            </Box>
         <FormLabel className="radio-label-1">Upload RFP</FormLabel>
         <label className="input">SELECT FILES
          <input type="file" className="file-upload" name="filename"></input>
          </label>
          <input type="submit" className="submit" value="SEND"></input>
             </div>
-            
-         
-        </div>
     );
 }
 export default form
