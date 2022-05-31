e240a3f31ad0ef8a7391c50b5510996287380eca
jaison080 <jaisondennis080@gmail.com>
2022-05-31 10:25:10 +0530

    Added typewriter effect on main heading

--- anile-media-website/components/Landing.js
+++ anile-media-website/components/Landing.js
@@ -1,12 +1,17 @@
 
-function Landing()
-{
-    return(
+function Landing() {
+    return (
         <div>
-            <div className="h1-landing">Where Future-Proof<br/>Strategy Meets Digital<br/>Innovation</div>
-        <div className="p-landing">Through unified <a href="/" >branding</a>, human-centered UX/UI <a href="/" >design</a>, future-<br/>forward <a href="/" >web development</a>, and strategic <a href="/" >digital marketing</a>, Big Drop<br/> works with the world’s most ambitious brands in creating innovative<br/> digital products designed to fuel innovation.</div>
-        <div className="logo"><img className="logo-img" src="https://img.icons8.com/ios/50/000000/expand-arrow--v1.png"/></div>
-        </div>
+            <h4>
+                <div className="c1"><div className="type">Where Future-Proof</div></div>
+                <br />
+                <div className="c2"><div className="type2">Strategy Meets Digital</div></div>
+                <br />
+                <div className="c3"><div className="type3">Innovation</div></div>
+            </h4 >
+            <div className="p-landing">Through unified <a href="/" >branding</a>, human-centered UX/UI <a href="/" >design</a>, future-<br />forward <a href="/" >web development</a>, and strategic <a href="/" >digital marketing</a>, Big Drop<br /> works with the world’s most ambitious brands in creating innovative<br /> digital products designed to fuel innovation.</div>
+            <div className="logo"><img className="logo-img" src="https://img.icons8.com/ios/50/000000/expand-arrow--v1.png" /></div>
+        </div >
     );
 }
 export default Landing
