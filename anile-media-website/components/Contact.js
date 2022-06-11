a23e687b9317bfc5848a76c58a25c97bac309826
jaison080 <jaisondennis080@gmail.com>
2022-06-11 09:46:04 +0530

    Added Carousel

--- anile-media-website/components/Contact.js
+++ anile-media-website/components/Contact.js
@@ -2,13 +2,6 @@ function Contact()
 {
     return(
         <div>
-            <div className="space">
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-            </div>
 <div className="contact-main">
             <div className="contact-2">Want to collaborate on a future-forward project?</div>
             <div className="contact-1">Let's Talk</div>
---
+++ anile-media-website/components/Slider.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css/sea-green';
+function Slider() {
+  return (
+    <Splide
+      options={ {
+        rewind: true,
+        gap:'1rem',
+        width:'100%',
+        height:'300px',
+        perPage:4,
+        type   : 'loop',
+        focus:'center'
+      } }
+      aria-label="My Favorite Images"
+    >
+      <SplideSlide>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" alt="Image 1"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" alt="Image 1"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" alt="Image 1"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" alt="Image 1"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" alt="Image 2"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" alt="Image 3"/>
+      </SplideSlide>
+    </Splide>
+  );
+}
+export default Slider
