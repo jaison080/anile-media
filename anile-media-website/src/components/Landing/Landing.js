7c00f7ae7275f0e186ab10a57591b5f3c1eb49a1
jaison080 <jaisondennis080@gmail.com>
2022-06-21 19:22:06 +0530

    updated

--- anile-media-website/src/components/Landing/Landing.js
+++ anile-media-website/src/components/Landing/Landing.js
@@ -2,13 +2,7 @@ import styles from './Landing.module.css'
 function Landing() {
     return (
         <div>
-            <h4 className={styles.h4}>
-                <div className={styles.c1}><div className={styles.type}>Where Future-Proof</div></div>
-                <br />
-                <div className={styles.c2}><div className={styles.type2}>Strategy Meets Digital</div></div>
-                <br />
-                <div className={styles.c3}><div className={styles.type3}>Innovation</div></div>
-            </h4 >
+            <div className={styles.h1_landing}>Where Future-Proof Strategy Meets Digital Innovation</div>
             <div className={styles.p_landing}>Through unified branding , human-centered UX/UI design , future-forward web development , and strategic digital marketing , Big Drop works with the world’s most ambitious brands in creating innovative digital products designed to fuel innovation.</div>
             <div className={styles.logo}><i class="fa fa-angle-double-down fa-2x" aria-hidden="true"></i></div>
         </div >
