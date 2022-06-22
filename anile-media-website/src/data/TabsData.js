b85a6ca2445acc1a6e881b77975910989cf5cdfb
jaison080 <jaisondennis080@gmail.com>
2022-06-22 19:32:15 +0530

    Updated

---
+++ anile-media-website/src/data/TabsData.js
@@ -0,0 +1,24 @@
+const TabsData=
+[
+    {
+        id:1,
+        title:"CREATIVE",
+        description:"Quality, consistency, and cadence are crucial to a successful content marketing operation. We lean on the power of all our creative ideas, both client and agency to find “right” based on consumer reactions vs guessing and having to be right with our opinions and subjectivity. The result? Powerful creative that works born from the consumer up.  Thus, we have unique insight into what works for you, what doesn’t, and what surprising things can drastically improve your results and up your game.",
+    },
+    {
+        id:2,
+        title:"MEDIA",
+        description:"We know you are addicted to social media but guess what? We are going to get you more addicted in most effective way. This means having a deep respect and intimate knowledge of channels where consumers are spending their time, and building modern planning, buying and analytics disciplines that drive consumers to act.  Targeting demographics, running alongside fads and trends, catering to users’ needs and acknowledging quality feedback along with potential improvements, we take care of it all.",
+    },
+    {
+        id:3,
+        title:"STRATEGY",
+        description:"We put human beings at the forefront of everything we do, ensuring our ideas and plans are culturally rich and consumer-led. Thought leadership, expertise and industry research delivered daily, weekly and monthly, we keep brands on cutting edge. We are as fanatical as you are about focusing on building relationships, cherishing success, scaling what works, testing, finding solutions and celebrating fruitful results louder than ever.",
+    },
+    {
+        id:4,
+        title:"NETWORKING",
+        description:"Whether you’re looking for a production house to film your latest video or a developer to code an interactive website, we have the skills and the expert referral to get it done and execute it for you. With an immense spider-web of networks that is a real game-changer with awareness and insight, we help you reach new audiences and create effective amplification for your brand communication.",
+    }
+]
+export default TabsData
