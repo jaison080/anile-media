1a6ce7b4b20c89363b4af76f3eb06905c17e1203
jaison080 <jaisondennis080@gmail.com>
2022-06-30 20:11:13 +0530

    Updated"

--- anile-media-website/src/data/TeamData.js
+++ anile-media-website/src/data/TeamData.js
@@ -4,11 +4,11 @@ const TeamData=
         id:1,
         name:"Havan Sharma",
         description:" Co-Founder , Marketing Strategist",
-        image:"",
+        image:"https://raw.githubusercontent.com/jaisondennis080/anile-media/main/anile-media-website/src/assets/avatar.jpg",
     },
     {
         id:2,
-        image:"",
+        image:"https://raw.githubusercontent.com/jaisondennis080/anile-media/main/anile-media-website/src/assets/avatar.jpg",
         name:"Sarvesh Pal",
         description:"Co-Founder , Business Strategist",
     },
